58338ec27ca53d79b5caa00457cceeb35d6ca0a4
Mark Fajet <markfajet@gmail.com>
2016-11-15 22:02:39 -0500

    I have a method that returns a list of all of the hamilton paths starting from a certain vertex.

--- nonmulti.py
+++ nonmulti.py
@@ -17,12 +17,14 @@ graph = {
 def find_h_path_to_v(g, v):
     used = {}
     path = [v]
+    paths={}
     for vertex in g:
         used[vertex] = 0
     used[v] = 1
     n = len(g)
     DNE = False
     i=0
+    counter = 0
 
     while len(path) < n or DNE:
         while i < len(g[v]):
@@ -30,13 +32,61 @@ def find_h_path_to_v(g, v):
             print i
             print v
             if len(path) == n:
-                return path
-            if used[vertex] == 0:
+                str_path = '-'.join(path)
+                print paths
+                if not (str_path in paths):
+                    counter+=1
+                    paths[str_path] = counter
+                    path.pop()
+            elif used[vertex] == 0:
                 used[vertex] = 1
                 path.append(vertex)
                 v = vertex
                 i=0
             else:
                 i+=1
+def find_paths_from_v(g, v):
+    start = v
+    n = len(g)
+    paths = []
+    path=[v]
+    used = {}
+    for vertex in g:
+        used[vertex] = 0
+    used[v] = 1
+    count_dict = {}
+    for w in g:
+        count_dict[w] = 0
+    while count_dict[v] < len(g[v]) or (v != start and count_dict[v] >= len(g[v])):
+        print v, count_dict, path
+        if v != start and count_dict[v] >= len(g[v]):
+            v = path.pop()
+            count_dict[v]=0
+            used[v] = 0
+            v = path[len(path)-1]
+        else:
+            vertex = g[v][count_dict[v]]
+            if len(path) == n:
+                str_path = '-'.join(path)
+                paths.append(str_path)
+                count_dict[v]=0
+                used[v] = 0
+                path.pop()
+                v = path[len(path)-1]
+                print str_path
 
-print find_h_path_to_v(graph, 'c')
+            elif used[vertex] == 0:
+                used[vertex] = 1
+                path.append(vertex)
+                count_dict[v]+=1
+                v = vertex
+
+            else:
+                count_dict[v]+=1
+
+        print v, count_dict[v]
+    return paths
+
+
+
+print find_paths_from_v(graph, 'a')
